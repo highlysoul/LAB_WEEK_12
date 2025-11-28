d78c9bac66942caade369d81faad5d92bd67a6ac
Fedora Anestasia <115520786+highlysoul@users.noreply.github.com>
2025-11-28 13:04:35 +0700

    Commit No. 2 – Building app with coroutines
    Building app with coroutines

--- app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
+++ app/src/main/java/com/example/test_lab_week_12/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.test_lab_week_12.model.Movie
 import com.google.android.material.snackbar.Snackbar
 import java.util.Calendar
+import com.google.android.material.appbar.MaterialToolbar
+
 
 class MainActivity : AppCompatActivity() {
     private val movieAdapter by lazy {
@@ -22,8 +24,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         val recyclerView: RecyclerView = findViewById(R.id.movie_list)
         recyclerView.adapter = movieAdapter
+
         val movieRepository = (application as MovieApplication).movieRepository
         val movieViewModel = ViewModelProvider(
             this, object : ViewModelProvider.Factory {
@@ -31,18 +38,18 @@ class MainActivity : AppCompatActivity() {
                     return MovieViewModel(movieRepository) as T
                 }
             })[MovieViewModel::class.java]
+
         movieViewModel.popularMovies.observe(this) { popularMovies ->
-            val currentYear =
-                Calendar.getInstance().get(Calendar.YEAR).toString()
+            val currentYear = Calendar.getInstance().get(Calendar.YEAR).toString()
             movieAdapter.addMovies(
                 popularMovies
                     .filter { movie ->
-// aman dari null
                         movie.releaseDate?.startsWith(currentYear) == true
                     }
                     .sortedByDescending { it.popularity }
             )
         }
+
         movieViewModel.error.observe(this) { error ->
             if (error.isNotEmpty()) {
                 Snackbar.make(recyclerView, error, Snackbar.LENGTH_LONG).show()
